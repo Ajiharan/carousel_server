cd3c33d6bd184cd3925d4fefd798f2763958d676
Ajiharan <baskaranajiharan1243@gmail.com>
2022-07-02 11:35:54 +0530

    initialize repo

---
+++ images.js
@@ -0,0 +1,62 @@
+export const slides = [
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxKhfTtSp78QuOvdqLhEzKoxJL83GYOxUxmFcuSYYul-dNCydjE983HtBuqzJk-OcsPSs&usqp=CAU",
+    title: "Light Mask",
+    subTitle: "subtitle 1",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDqT4w2Bxqns1A5uxDQHydgEixmAySMgVBAQiseKfY1C_SY1Q7Gagq8TvQoAi2dwAaHn8&usqp=CAU",
+    title: "Title 2",
+    subTitle: "subtitle 2",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW9fiiDaybFia1CdkwLMUlmQnT8cI0jagFmSFVpJQF7-GSAXKp8nypXdYTTHBW_HTHeBU&usqp=CAU",
+    title: "Title 3",
+    subTitle: "subtitle 3",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT6R6bjk11fol5DQC87zR-vDLrUNxa4Bqn9RdQC0bMjvd-_YQhBDRx_anvUZ-LuRGBp2k&usqp=CAU",
+    title: "Title 4",
+    subTitle: "subtitle 4",
+  },
+  {
+    image:
+      "https://mainwebstorage.blob.core.windows.net/mediacontainers/styles/1920x1400/azure/2022-05/Koenigsegg%20Gemera_01%20copy.jpg?h=2e930402&itok=YveuLY7M",
+    title: "Title 5",
+    subTitle: "subtitle 5",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLawsUdSIJiyPE3D07JsKNoax0bZfaFBF5aKEb1m7dHg9toaLT-uz58G4wWnKlqa2K2Fw&usqp=CAU",
+    title: "Title 6",
+    subTitle: "subtitle 6",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ76blIlcZzGreoOw-hyFQs7DCNT84THPtj8b42OR9wxgDrI6iyhBoY3PlRnw7HAXcd7Oc&usqp=CAU",
+    title: "Title 7",
+    subTitle: "subtitle 7",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvjwicIF9OrR-vLpCLElMRRtlmCIOkCUwfo5CMP2OdEQ-x49U88UaN0mQ2IpdjNgOB_40&usqp=CAU",
+    title: "Title 8",
+    subTitle: "subtitle 8",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlV9hNogh4LoibIxF4eCAcikOgAli_KrQC73yaH-aDdtsFQCvys0vRv2uWl8aA5CUKX2o&usqp=CAU",
+    title: "Title 9",
+    subTitle: "subtitle 9",
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjhdTjrGFoeVDwPOtuH3vUGiHc4mkKzLy9TrN7ALjaGaUk_ixpv1lnrut9EB4VIBaJ_ic&usqp=CAU",
+    title: "Title 10",
+    subTitle: "subtitle 10",
+  },
+];
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { slides } from "./images.js";
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/api/carousel", (req, res) => {
+  console.log(req.query);
+
+  const count = req.query.slides || 0;
+  const newSlides = slides.slice(0, count);
+
+  res.status(200).json(newSlides);
+});
+
+app.listen(PORT, () => {
+  console.log(`port listen in ${PORT}`);
+});
